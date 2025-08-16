0072884e10dd5fe89c02e2932a53b7e305ec7973
John Chen <wishingwork@yahoo.com.tw>
2025-08-16 10:08:23 +0800

    🚧 Clear weather icon

--- constants/weather.js
+++ constants/weather.js
@@ -5,6 +5,10 @@ export const WEATHER_CONDITIONS = {
     label: i18n.t("weather_sunny"),
     icon: require("../assets/images/weather-sunny.png"),
   },
+  Clear: {
+    label: i18n.t("weather_clear"),
+    icon: require("../assets/images/weather-sunny.png"),
+  },  
   Clouds: {
     label: i18n.t("weather_cloudy"),
     icon: require("../assets/images/weather-rainy.png"),
